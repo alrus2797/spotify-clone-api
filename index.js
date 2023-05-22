1215ecea27286a97e97de596d1a1c910be723df5
Alexander Apaza <alrus2797@gmail.com>
2023-05-22 17:31:07 -0500

    feat: add  module configuration

---
+++ index.js
@@ -0,0 +1,21 @@
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
+const app = jsonServer.create()
+const router = jsonServer.router({
+  categories: require('./db_files/categories.json'),
+  sections: require('./db_files/sections.json'),
+  users: []
+})
+
+const port = process.env.PORT || 3000
+
+// /!\ Bind the router db to the app
+app.db = router.db
+
+// You must apply the auth middleware before the router
+app.use(auth)
+app.use(router)
+app.listen(port, () => {
+  console.log('App started on', port)
+})
